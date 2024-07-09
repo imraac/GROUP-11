846affdbe874ff3c00b7f5a95a5ae0fe7b527654
user.name <akamrannicole@gmail.com>
2024-07-09 13:25:21 +0300

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
-
+import Register from './components/Register';
 
 function App() {
   return (
-    <div>
-      <Login/>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
