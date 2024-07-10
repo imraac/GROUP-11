f5fed4e5e8733fb434123caf56b53009b0ee7c49
user.name <akamrannicole@gmail.com>
2024-07-10 11:12:00 +0300

    forgotpassword done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import Navbar from './components/NavBar';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
 
 function App() {
   return (
@@ -10,8 +11,9 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/" element={<Register />} />
-        <Route path="/" element={<Navbar />} />
       </Routes>
     </Router>
   );
